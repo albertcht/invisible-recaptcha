a1a2d628a989f3452ab28fdff96b50a6e645e83f
Albert Chen <albert.cht@gmail.com>
2017-08-17 22:12:34 +0800

    remove debug config

--- src/config/captcha.php
+++ src/config/captcha.php
@@ -3,6 +3,5 @@
 return [
     'siteKey' => env('INVISIBLE_RECAPTCHA_SITEKEY'),
     'secretKey' => env('INVISIBLE_RECAPTCHA_SECRETKEY'),
-    'hideBadge' => env('INVISIBLE_RECAPTCHA_BADGEHIDE', false),
-    'debug' => env('INVISIBLE_RECAPTCHA_DEBUG', false)
+    'hideBadge' => env('INVISIBLE_RECAPTCHA_BADGEHIDE', false)
 ];
